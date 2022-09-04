4bb3c6dfff8a716f841c194dd8a20105780a961c
Andy <3775@holbertonschool.com>
2022-09-04 19:34:43 -0400

    Try David's base_model

--- models/state.py
+++ models/state.py
@@ -10,14 +10,3 @@ class State(BaseModel, Base):
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship('City', back_populates='state', cascade='all, delete')
-
-    @property
-    def cities_att(self):
-        """Defines cities attribute for FileStorage"""
-        from models import storage
-        cities_dict = storage.all('City')
-        cities_list = []
-        for key, value in cities_dict.items():
-            if value.state_id == self.id:
-                cities_list.append(value)
-        return cities_list
