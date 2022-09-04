46787d47f6af7f6ef42f3c4240f45d27e656bc95
Andy <3775@holbertonschool.com>
2022-09-04 19:59:41 -0400

    David's

---
+++ models/engine/db_storage_.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+"""Module handling database storage"""
+import os
+from typing_extensions import Self
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
+from models.base_model import BaseModel
+from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
+
+
+# Set these internally for testing and development
+# HBNB_ENV = 'dev'
+# HBNB_MYSQL_USER = 'hbnb_dev'
+# HBNB_MYSQL_PWD = 'hbnb_dev_pwd'
+# HBNB_MYSQL_HOST = 'localhost'
+# HBNB_MYSQL_DB = 'hbnb_dev_db'
+
+# Set these with environmental variables for project requirements
+HBNB_ENV = os.getenv('HBNB_ENV')
+HBNB_MYSQL_USER = os.getenv('HBNB_MYSQL_USER')
+HBNB_MYSQL_PWD = os.getenv('HBNB_MYSQL_PWD')
+HBNB_MYSQL_HOST = os.getenv('HBNB_MYSQL_HOST')
+HBNB_MYSQL_DB = os.getenv('HBNB_MYSQL_DB')
+
+
+classes = {
+            'State': State, 'City': City
+          }
+
+
+class DBStorage:
+    """Class defining database storage object"""
+
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """function to run during DBstorage instance creation"""
+        conn = f"mysql+mysqldb://{HBNB_MYSQL_USER}:{HBNB_MYSQL_PWD}@\{HBNB_MYSQL_HOST}/{HBNB_MYSQL_DB}"
+        self.__engine = create_engine(conn, pool_pre_ping=True, echo=False)
+        if HBNB_ENV == 'test':
+            Base.metadata.drop_all(self.__engine)
+
+
+    def reload(self):
+        """Bring database into application as objects"""
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+
+
+    def new(self, obj):
+        """Add object to current session"""
+        self.__session.add(obj)
+
+
+    def save(self):
+        self.__session.commit()
+
+    def delete(self, obj):
+        """delete object from current session"""
+        self.__session.delete(obj)
+
+    def all(self, cls=None):
+        """Returns a dictionary of models currently in storage"""
+        results = {}
+        if cls is None:
+            for k, v in classes.items():
+                objs = self.__session.query(v).all()
+                for obj in objs:
+                    results[f"{k}.{obj.id}"] = obj
+        else:
+            print(type(self.__session))
+            objs = self.__session.query(cls).all()
+            for obj in objs:
+                results[f"{k}.{obj.id}"] = obj
+        return results
--- models/state.py
+++ models/state.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, Integer
+import os
+from models.base_model import BaseModel
+from models.base_model import Base
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """ State class """
-    __tablename__ = 'states'
+    __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    cities = relationship('City', back_populates='state', cascade='all, delete')
+
+    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship("City", backref='State', cascade="delete")
+    else:
+        @property
+        def cities_att(self):
+            """Defines cities attribute for FileStorage"""
+            from models import storage
+            cities_dict = storage.all('City')
+            cities_list = []
+            for key, value in cities_dict.items():
+                if value.state_id == self.id:
+                    cities_list.append(value)
+            return cities_list
