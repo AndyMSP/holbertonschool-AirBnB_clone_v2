c901139cbd879daa62645b764f15d8ef77d52639
Andy <3775@holbertonschool.com>
2022-09-04 21:19:46 -0400

    afsgsadf

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -25,11 +25,16 @@ from curses import echo
 # HBNB_MYSQL_DB = 'hbnb_dev_db'
 
 # Set these with environmental variables for project requirements
-HBNB_ENV = os.getenv('HBNB_ENV')
-HBNB_MYSQL_USER = os.getenv('HBNB_MYSQL_USER')
-HBNB_MYSQL_PWD = os.getenv('HBNB_MYSQL_PWD')
-HBNB_MYSQL_HOST = os.getenv('HBNB_MYSQL_HOST')
-HBNB_MYSQL_DB = os.getenv('HBNB_MYSQL_DB')
+# HBNB_ENV = os.getenv('HBNB_ENV')
+# HBNB_MYSQL_USER = os.getenv('HBNB_MYSQL_USER')
+# HBNB_MYSQL_PWD = os.getenv('HBNB_MYSQL_PWD')
+# HBNB_MYSQL_HOST = os.getenv('HBNB_MYSQL_HOST')
+# HBNB_MYSQL_DB = os.getenv('HBNB_MYSQL_DB')
+
+os.environ['HBNB_MYSQL_USER'] = 'hbnb_dev'
+os.environ['HBNB_MYSQL_PWD'] = 'hbnb_dev_pwd'
+os.environ['HBNB_MYSQL_HOST'] = 'localhost'
+os.environ['HBNB_MYSQL_DB'] = 'hbnb_dev_db'
 
 
 classes = {
@@ -43,11 +48,23 @@ class DBStorage:
     __engine = None
     __session = None
 
+    # def __init__(self):
+    #     """function to run during DBstorage instance creation"""
+    #     conn = f"mysql+mysqldb://{HBNB_MYSQL_USER}:{HBNB_MYSQL_PWD}@\{HBNB_MYSQL_HOST}/{HBNB_MYSQL_DB}"
+    #     self.__engine = create_engine(conn, pool_pre_ping=True, echo=False)
+    #     if HBNB_ENV == 'test':
+    #         Base.metadata.drop_all(self.__engine)
+
+
     def __init__(self):
-        """function to run during DBstorage instance creation"""
-        conn = f"mysql+mysqldb://{HBNB_MYSQL_USER}:{HBNB_MYSQL_PWD}@\{HBNB_MYSQL_HOST}/{HBNB_MYSQL_DB}"
-        self.__engine = create_engine(conn, pool_pre_ping=True, echo=False)
-        if HBNB_ENV == 'test':
+        """Engine"""
+        self.__engine = db.create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            os.getenv('HBNB_MYSQL_USER'),
+            os.getenv('HBNB_MYSQL_PWD'),
+            os.getenv('HBNB_MYSQL_HOST'),
+            os.getenv('HBNB_MYSQL_DB')),
+            pool_pre_ping=True)
+        if os.getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
 
 
