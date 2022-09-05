04038dd97d45c69c959bee51370a46a7ec6da074
Andy <3775@holbertonschool.com>
2022-09-04 21:30:54 -0400

    gdafdsg

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -2,7 +2,7 @@
 """Module handling database storage"""
 # import os
 # from typing_extensions import Self
-# from sqlalchemy import create_engine
+from sqlalchemy import create_engine
 # from sqlalchemy.orm import sessionmaker, scoped_session
 # from models.base_model import Base
 # from models.base_model import BaseModel
