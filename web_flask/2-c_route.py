f5d5388be4bea43ce8ad809b3f5ece07ab126f59
Andy <3775@holbertonschool.com>
2022-09-11 15:34:18 -0400

    Add task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Starts a basic flask web application"""
+
+from flask import Flask
+from markupsafe import escape
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def hello_hbnb():
+    """Function to run when '/' is accessed"""
+    return ("Hello HBNB!")
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """Function to run when '/hbnb' is accessed"""
+    return ("HBNB")
+
+
+@app.route("/c/<text>")
+def c_text(text):
+    """Function to run when '/c/<text>' is accessed"""
+    return (f"C {text.replace('_', ' ')}")
+
+
+if (__name__ == '__main__'):
+    app.run(host='0.0.0.0', port=5000, debug=False)
