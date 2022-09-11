33130513f805d3120680e6d9e288ab37848d07d2
Andy <3775@holbertonschool.com>
2022-09-11 15:54:06 -0400

    Add task 3

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -23,6 +23,12 @@ def c_text(text):
     """Function to run when '/c/<text>' is accessed"""
     return ("C {}".format(text.replace('_', ' ')))
 
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_text(text="is cool"):
+    """Function to run when /python/<text>' is accessed"""
+    return("Python {}".format(text.replace('_', ' ')))
+
 
 if (__name__ == '__main__'):
     app.run(host='0.0.0.0', port=5000, debug=False)
