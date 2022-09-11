e14ca84ef42a83b74af3524be6690934f01eed37
Andy <3775@holbertonschool.com>
2022-09-11 15:05:58 -0400

    Pycodestyle

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -4,11 +4,11 @@
 from flask import Flask
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def home():
     """Function to run when '/' is accessed"""
     return ("Hello HBNB!")
-
 
 
 if (__name__ == '__main__'):
