2ffa20515706e57247ece1a1534770ac5839053c
Andy <3775@holbertonschool.com>
2022-09-11 23:32:57 -0400

    debug

--- console.py
+++ console.py
@@ -163,7 +163,7 @@ class HBNBCommand(cmd.Cmd):
 
         key = c_name + "." + c_id
         try:
-            print(storage.all[key])
+            print(storage.all()[key])
         except KeyError:
             print("** no instance found **")
 
