a7a90fd4f1e85d6cc76a4ce56e447b144364a056
Andy <3775@holbertonschool.com>
2022-09-13 19:25:06 -0400

    include states_list

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -10,8 +10,17 @@ from models.city import City
 app = Flask(__name__)
 
 
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    """Function to run when '/states_list' is accessed"""
+    states = [state for state in storage.all(State).values()]
+    storage.close()
+    states.sort(reverse=False, key=lambda state: state.name)
+    return (render_template('7-states_list.html', states=states))
+
+
 @app.route("/cities_by_states", strict_slashes=False)
-def hello_hbnb():
+def cities_by_statesb():
     """Function to run when '/cities_by_states' is accessed"""
     states = storage.all(State).values()
     page = render_template('8-cities_by_states.html', states=states)
